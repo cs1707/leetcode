b3b5446345370f4f623d8590b81fa2cdaefd90e5
cs1707 <lost1q84@gmail.com>
2016-03-20 19:19:46 +0800

    commited by leetcode-ext

--- 2-Add-Two-Numbers/solution.js
+++ 2-Add-Two-Numbers/solution.js
@@ -11,5 +11,35 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    
+    var v1 = 0
+    var v2 = 0
+    var sum = 0
+    var carry = 0
+    var l3
+    while(l1 != null || l2 != null) {
+        if(l1) {
+            v1 = l1.val
+            l1 = l1.next
+        }
+        
+        if(l2) {
+            v2 = l2.val
+            l2 = l2.next
+        }
+        
+        sum = carry + v1 + v2
+        var node = new ListNode(sum % 10)
+        if(!l3) {
+            l3 = node
+        } else {
+            l3.next = node
+        }
+        
+        carry = Math.floor(sum / 10)
+    }
+    if(carry > 0) {
+        l3.next = new ListNode(carry)
+    }
+    return l3
 };
+
