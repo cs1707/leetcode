0406b700bb9388883781088fa8338675409fabb3
cs1707 <lost1q84@gmail.com>
2016-03-22 08:07:31 +0800

    init

--- 3-Longest-Substring-Without-Repeating-Characters/solution.js
+++ 3-Longest-Substring-Without-Repeating-Characters/solution.js
@@ -3,5 +3,22 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
+    var last = -1
+    var map = {}
+    var max = 0
     
+    for(var i = 0; i < s.length; i++) {
+        var char = s[i]
+        if(map[char] != null && last <= map[char]) {
+            last = map[char]
+        }
+        
+        if(i - last > max) {
+            max = i - last
+        }
+        
+        map[char] = i
+    }
+    
+    return max
 };
