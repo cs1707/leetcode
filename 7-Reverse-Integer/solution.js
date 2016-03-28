ef9f335963e14110488a60fb3a574e9d4d9240e3
cs1707 <lost1q84@gmail.com>
2016-03-28 23:08:07 +0800

    [7. Reverse Integer][Runtime Error]committed by LeetCode Extension

--- 7-Reverse-Integer/solution.js
+++ 7-Reverse-Integer/solution.js
@@ -3,9 +3,17 @@
  * @return {number}
  */
 var reverse = function(x) {
+    var MAX = 2147483647 // 2 ^ 31 - 1
+    var MIN = -2147483648 // 2 ^ 31
     var r = 0
+    var n 
     while (x !== 0) {
-        r = r * 10 + x % 10
+        // 检查溢出
+        n = x % 10
+        if ((MAX - n) / 10) > r || (MIN - n) / 10 < r) {
+            return 0
+        }
+        r = r * 10 + n
         x = ~~(x / 10)
     }
     return r
