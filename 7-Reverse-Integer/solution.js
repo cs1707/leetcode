55b8a6740ce458d68bf00543320979205628ef65
cs1707 <lost1q84@gmail.com>
2016-03-28 23:13:12 +0800

    [7. Reverse Integer][Accepted]committed by LeetCode Extension

--- 7-Reverse-Integer/solution.js
+++ 7-Reverse-Integer/solution.js
@@ -10,6 +10,10 @@ var reverse = function(x) {
     while (x !== 0) {
         // 检查溢出
         n = x % 10
+        /*
+            r * 10 + n > MAX      =>     (MAX - n) / 10 < r
+            r * 10 + n < MIN      =>     (MIN - n) / 10 > r
+        */
         if ((MAX - n) / 10 < r || (MIN - n) / 10 > r) {
             return 0
         }
