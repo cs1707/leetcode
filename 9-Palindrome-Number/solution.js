a36e0eb0a95c1f0fd12d025e8adc074420d33c6d
cs1707 <lost1q84@gmail.com>
2016-03-31 06:50:03 +0800

    [9. Palindrome Number][Time Limit Exceeded]committed by LeetCode Extension

--- 9-Palindrome-Number/solution.js
+++ 9-Palindrome-Number/solution.js
@@ -15,13 +15,13 @@ var isPalindrome = function(x) {
     }
     
     while (x !== 0) {
-        var left = parseInt(x / len) // 最高位数字
+        var left = ~~(x / len) // 最高位数字
         var right = x % 10 // 个位数字
         if (left !== right) {
             return false
         }
         
-        x = parseInt(x % len / 10) // 去除最高位和个位数字
+        x = ~~(x % len / 10) // 去除最高位和个位数字
         len /= 100
     }
     return true
