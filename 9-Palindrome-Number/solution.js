ff86ca8b0acf1481429d34b4246788178f879408
cs1707 <lost1q84@gmail.com>
2016-03-31 06:55:39 +0800

    Update solution.js

--- 9-Palindrome-Number/solution.js
+++ 9-Palindrome-Number/solution.js
@@ -2,26 +2,23 @@
  * @param {number} x
  * @return {boolean}
  */
-/*  反转数字比较是否相当
-    没有考虑溢出……
-*/
 var isPalindrome = function(x) {
     if (x < 0) {
         return false
     }
     var len = 1
-    while (parseInt(x / 10) >= 10) {
+    while (parseInt(x / len) >= 10) {
         len *= 10
     }
     
     while (x !== 0) {
-        var left = ~~(x / len) // 最高位数字
+        var left = parseInt(x / len) // 最高位数字
         var right = x % 10 // 个位数字
         if (left !== right) {
             return false
         }
         
-        x = ~~(x % len / 10) // 去除最高位和个位数字
+        x = parseInt(x % len / 10) // 去除最高位和个位数字
         len /= 100
     }
     return true
