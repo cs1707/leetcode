7ceda2c31c8dc41fafb7e0fac59bb3193812426e
cs1707 <lost1q84@gmail.com>
2016-03-31 06:23:46 +0800

    [9. Palindrome Number][Wrong Answer]committed by LeetCode Extension

---
+++ 9-Palindrome-Number/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false
+    }
+    var r = 0
+    var y = x
+    while (y !== 0) {
+        r = r * 10 + x % 10
+        y = parseInt(y / 10)
+    }
+    if (r === y) {
+        return true
+    }
+    return false
+};
