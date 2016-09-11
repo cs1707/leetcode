6153b0e90a0e23aa36c085266b43f56aba98677d
cs1707 <lost1q84@gmail.com>
2016-09-11 20:16:44 +0800

    [389. Find the Difference][Accepted]committed by LeetCode Extension

--- 389-Find-the-Difference/solution.js
+++ 389-Find-the-Difference/solution.js
@@ -7,10 +7,12 @@ var findTheDifference = function(s, t) {
     var sum1 = 0
     var sum2 = 0
     for (var i = 0; i < s.length; i++) {
-        sum1 += s[i].charCodeat()
+        sum1 += s[i].charCodeAt()
     }
-    for (var i = 0; i < t.length; i++) {
-        sum1 += t[i].charCodeat()
+    
+    for (var j = 0; j < t.length; j++) {
+        sum2 += t[j].charCodeAt()
     }
+    
     return String.fromCharCode(sum2 - sum1)
 };
