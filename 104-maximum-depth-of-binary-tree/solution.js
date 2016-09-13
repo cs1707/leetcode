ac455891fb449b3894bef3a396c6eebccfb58be9
xiao peng <lost1q84@gmail.com>
2016-09-13 13:26:18 +0800

    [104. Maximum Depth of Binary Tree][Runtime Error]

---
+++ 104-maximum-depth-of-binary-tree/solution.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    
+    if (!root) {
+        return 0
+    }
+    
+    var num = 1
+    travel(root, 1)
+    
+    return num
+    
+    function travel(root, level) {
+        if(!root.left && !root.right) {
+            num = Math.max(num, level)
+            return
+        }
+        
+        if (root.left) {
+            travel(root.left, level + 1)
+        }
+        
+        if (root.right) {
+            travel(root.left, level + 1)
+        }
+    }
+};
+
