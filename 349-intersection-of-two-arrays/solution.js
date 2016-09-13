9f2a20caf14bdc2fd30d47e39f2e7e34b938f500
xiao peng <lost1q84@gmail.com>
2016-09-13 11:03:54 +0800

    [349. Intersection of Two Arrays][Runtime Error]

---
+++ 349-intersection-of-two-arrays/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    var map1 = convertToMap(nums1)
+    var map2 = convertToMap(nums2)
+    var result = []
+    for (var num in map1) {
+        if (map[num]) {
+            result.push(num)
+        }
+    }
+    return result
+};
+
+function convertToMap(arr) {
+    var map = {}
+    for (var i = 0; i < arr.length; i++) {
+        map[i] = true
+    }
+    return map
+}
