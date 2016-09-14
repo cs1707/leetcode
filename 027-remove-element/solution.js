fa2a51fc5c89079e755162ff88af73e2d8d9ccdf
xiao peng <lost1q84@gmail.com>
2016-09-14 13:43:45 +0800

    [27. Remove Element][Accepted]

--- 027-remove-element/solution.js
+++ 027-remove-element/solution.js
@@ -7,10 +7,10 @@ var removeElement = function(nums, val) {
     var count = 0
     for (var i = 0; i < nums.length; i++) {
         if (nums[i] !== val) {
-            a[count] = nums[i]
+            nums[count] = nums[i]
             count++
         }
     }
-    nums.length = count + 1
-    return count + 1
+    nums.length = count
+    return nums.length
 };
