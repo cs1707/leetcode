b8bf042679ae0b8d8dc385516b2f508203cab5a2
xiao peng <lost1q84@gmail.com>
2016-09-14 14:40:24 +0800

    [28. Implement strStr()][Accepted]

--- 028-implement-strstr()/solution.js
+++ 028-implement-strstr()/solution.js
@@ -11,6 +11,7 @@ var strStr = function(haystack, needle) {
     if (needle.length >= haystack.length) {
         return -1
     }
+    
     for (var i = 0; i < haystack.length; i++) {
         if (haystack.substring(i, i + needle.length) === needle) {
             return i
