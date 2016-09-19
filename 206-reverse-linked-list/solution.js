7df7e1f1eddfebefc927e84aec58af02f7442eb2
xiao peng <lost1q84@gmail.com>
2016-09-19 11:24:59 +0800

    [206. Reverse Linked List][Runtime Error]

--- 206-reverse-linked-list/solution.js
+++ 206-reverse-linked-list/solution.js
@@ -12,11 +12,12 @@
 var reverseList = function(head) {
     var node = head
     while (node) {
+        head = node.next
+        head.next = node
+        
         if (node.next) {
-            var next = node.next
-            head = node.next
-            head.next = node
-            node.next = next.next
+            var next = node.next.next
+            node.next = next
         }
         node = node.next
     }
