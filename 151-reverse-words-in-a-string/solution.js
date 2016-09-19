f386d8ed40b6236427ecb0088ce20f2643c0e15b
xiao peng <lost1q84@gmail.com>
2016-09-19 16:19:54 +0800

    [151. Reverse Words in a String][Accepted]

--- 151-reverse-words-in-a-string/solution.js
+++ 151-reverse-words-in-a-string/solution.js
@@ -4,19 +4,18 @@
  */
 var reverseWords = function(str) {
     var result = []
-    var start = 0
     var word = ''
     for (var i = 0; i < str.length; i++) {
-        if (str[i] === ' ' || i === str.length - 1) {
-            word = str.slice(start, i + 1)
+        if (str[i] !== ' ') {
+            word += str[i]
         }
-        if (word && word !== ' ') {
-            result.push(word)
+        if (str[i] === ' ' || i === str.length - 1){
+            if (word) {
+                result.unshift(word)
+            }
+            
+            word = ''
         }
     }
-    var resultStr = result[0] || ''
-    for (var j = 1; j < result.length; j++) {
-        resultStr = result[i] + ' ' + resultStr
-    }
-    return resltStr
+    return result.join(' ')
 };
