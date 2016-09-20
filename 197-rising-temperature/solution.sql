8baed406e732dceb6bbc4a3a12fabf6db16e1c26
xiao peng <lost1q84@gmail.com>
2016-09-20 16:48:01 +0800

    [197. Rising Temperature][Accepted]

--- 197-rising-temperature/solution.sql
+++ 197-rising-temperature/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT Id From Weather a
+SELECT a.Id From Weather a
 INNER JOIN Weather b
 ON DATE_SUB(a.Date, INTERVAL 1 DAY) = b.DATE
 AND a.Temperature > b.Temperature;
