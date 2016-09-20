62661d05e48428f092a7b5d41d0823f49f4f12e3
xiao peng <lost1q84@gmail.com>
2016-09-20 14:33:23 +0800

    [196. Delete Duplicate Emails][Runtime Error]

--- 196-delete-duplicate-emails/solution.sql
+++ 196-delete-duplicate-emails/solution.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-DELETE FROM Person
-WHERE Id NOT IN (SELECT Id FROM Person GROUP BY Id ORDER BY Id ASC);
+DELETE FROM Person a
+WHERE a.Id NOT IN (SELECT b.Id FROM Person b GROUP BY Id ORDER BY Id ASC);
