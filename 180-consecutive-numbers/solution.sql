66e42086a4eebc81a8cdd008814b6983cd02bb58
xiao peng <lost1q84@gmail.com>
2016-09-21 13:04:24 +0800

    [180. Consecutive Numbers][Accepted]

--- 180-consecutive-numbers/solution.sql
+++ 180-consecutive-numbers/solution.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
-SELECT a.Id FROM Logs a
+SELECT a.Num AS ConsecutiveNums FROM (Logs a
 INNER JOIN Logs b
-ON a.Id + 1 = b.Id
+ON a.Id + 1 = b.Id)
 INNER JOIN Logs c
-ON a.Id + 2 = c.Id
-GROUP BY Num;
+ON b.Id + 1 = c.Id
+WHERE a.Num = b.Num AND a.Num = c.Num
+GROUP BY a.Num;
